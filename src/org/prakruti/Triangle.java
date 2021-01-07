aefb61671a5bb98c1793197483b4e44027279f4e
PrakrutiChandak <prakrutichandak@gmail.com>
2021-01-08 00:14:52 +0530

    injecting objects

--- src/org/prakruti/Triangle.java
+++ src/org/prakruti/Triangle.java
@@ -2,32 +2,52 @@ package org.prakruti;
 
 public class Triangle {
 
-	private String type;
-	private int height;
-	
-	public int getHeight() {
-		return height;
+	private Point pointA;
+	private Point pointB;
+	private Point pointC;
+
+	public Point getPointA() {
+		return pointA;
 	}
 
-	public Triangle(String type) {
-		this.type = type;
-	}
-	
-	public Triangle(String type, int height) {
-		this.type = type;
-		this.height = height;
-	}
-	
-	public String getType() {
-		return type;
+
+
+	public void setPointA(Point pointA) {
+		this.pointA = pointA;
 	}
 
-	public void setType(String type) {
-		this.type = type;
+
+
+	public Point getPointB() {
+		return pointB;
 	}
+
+
+
+	public void setPointB(Point pointB) {
+		this.pointB = pointB;
+	}
+
+
+
+	public Point getPointC() {
+		return pointC;
+	}
+
+
+
+	public void setPointC(Point pointC) {
+		this.pointC = pointC;
+	}
+
+
 
 	public void draw() {
-		System.out.println(getType() + " triangle drawn of height " + getHeight());
+		
+		System.out.println("Point a = (" + getPointA().getX() +"," + getPointA().getY() + ")");
+		System.out.println("Point b = (" + getPointB().getX() +"," + getPointB().getY() + ")");
+		System.out.println("Point c = (" + getPointC().getX() +"," + getPointC().getY() + ")");
+		
 	}
 
 }
