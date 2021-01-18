bf83e43be1dbad931c91daded5712e1f64f53e9a
PrakrutiChandak <prakrutichandak@gmail.com>
2021-01-18 17:47:41 +0530

    bean scopes

---
+++ Readme.md
@@ -0,0 +1,10 @@
+# Bean Scopes
+* Singleton - the beans are created only once per spring container. it is by default scope type.
+* Prototype - new beans are created with every request or reference   
+  Example: <bean id="triangle" class="org.prakruti.Triangle" autowire="byName" scope="prototype>
+* Web-aware Context bean scopes
+    * Request -  new bean per servlet request
+    * Session - new bean per session. a bean per user per session
+    * Global Session - new bean per global HTTP session
+ 
+ 
--- src/org/prakruti/Triangle.java
+++ src/org/prakruti/Triangle.java
@@ -3,22 +3,39 @@ package org.prakruti;
 import java.util.List;
 
 public class Triangle {
-
-	private List<Point> points;
-
-	public List<Point> getPoints() {
-		return points;
+	private Point pointA;
+	private Point pointB;
+	private Point pointC;
+	
+	public Point getPointA() {
+		return pointA;
 	}
 
-	public void setPoints(List<Point> points) {
-		this.points = points;
+	public void setPointA(Point pointA) {
+		this.pointA = pointA;
+	}
+
+	public Point getPointB() {
+		return pointB;
+	}
+
+	public void setPointB(Point pointB) {
+		this.pointB = pointB;
+	}
+
+	public Point getPointC() {
+		return pointC;
+	}
+
+	public void setPointC(Point pointC) {
+		this.pointC = pointC;
 	}
 
 	public void draw() {
+		System.out.println("Point a = (" + pointA.getX() +"," + pointA.getY() + ")");
+		System.out.println("Point b = (" + pointB.getX() +"," + pointB.getY() + ")");
+		System.out.println("Point c = (" + pointC.getX() +"," + pointC.getY() + ")");
 
-		for (Point point : points) {
-			System.out.println("Point = (" + point.getX() + "," + point.getY() + ")");
-		}
 	}
 
 }
