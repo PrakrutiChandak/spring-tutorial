125d61434e7829cd2adbb152743d80617788dc76
PrakrutiChandak <prakrutichandak@gmail.com>
2021-01-19 00:18:06 +0530

    bean post processor

---
+++ src/org/prakruti/DisplayNameBeanPostProcessor.java
@@ -0,0 +1,20 @@
+package org.prakruti;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+
+public class DisplayNameBeanPostProcessor implements BeanPostProcessor {
+
+	@Override
+	public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+		System.out.println("In After Initialization menthod. Bean Name is:" + beanName);
+		return bean;
+	}
+
+	@Override
+	public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+		System.out.println("In Before Initialization menthod. Bean Name is:" + beanName);
+		return bean;
+	}
+
+}
